e37546848554be2b957d79b9edcd39176c991b2f
thedon122 <donovan122@yahoo.com>
2021-01-11 23:54:30 -0500

    first parts

---
+++ basketball-app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import base64
+
+st.title('NBA Player Stats Explorer')
+
+st.markdown("""
+This app performs simple webscraping of NBA player stats data!
+* **Python libraries:** base64, pandas, streamlit
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+""")
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
