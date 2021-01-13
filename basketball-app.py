e6cb71014a6a0c34e898ff80ae06588eff7a27a6
thedon122 <donovan122@yahoo.com>
2021-01-12 19:37:57 -0500

    load data from web scraping

--- basketball-app.py
+++ basketball-app.py
@@ -12,3 +12,17 @@ This app performs simple webscraping of NBA player stats data!
 
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
+
+
+# Web scraping of NBA player stats
+@st.cache
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+playerstats = load_data(selected_year)
+
